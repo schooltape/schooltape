ddfc11d3ed869f206ada37846082e81b1319a36f
42willow <102953226+schooltape@users.noreply.github.com>
2023-03-30 07:18:49 +1100

    Tab Title Plugin #10

--- Extension/Plugins/tab-title/tab-title.js
+++ Extension/Plugins/tab-title/tab-title.js
@@ -1,2 +1,38 @@
 const tabTitleConsoleStyle = "color: lime; font-weight: bold;";
 console.log(`%c[tab-title.js]`, tabTitleConsoleStyle, "Injected tab-title.js!");
+
+
+if (document.getElementById("heading")) {
+    // change the title of the tab to the value attribute of #heading
+    document.title = document.getElementById("heading").getAttribute("value");
+} else if (window.location.pathname === "/") { // check if the current page is the homepage
+    // change the title of the tab to "Homepage"
+    document.title = "Homepage";
+} else if (window.location.pathname === "/timetable") { // check if the current page is the timetable page
+    // change the title of the tab to "Timetable"
+    document.title = "Timetable";
+} else if (window.location.pathname === "/calendar") { // check if the current page is the calendar page
+    // change the title of the tab to "Calendar"
+    document.title = "Calendar";
+} else if (window.location.pathname === "/news") { // check if the current page is the news page
+    // change the title of the tab to "News"
+    document.title = "News";
+} else if (window.location.pathname === "/learning/classes") { // check if the current page is the classes page
+    // change the title of the tab to "Classes"
+    document.title = "Classes";
+} else if (window.location.pathname.includes("/grades/")) {
+    // change the title of the tab to "Grades"
+    document.title = "Grades";
+} else if (window.location.pathname.includes("/news/")) { // check if the current page is a news page
+    // change the title of the tab to "News"
+    document.title = "News";
+    // append the title of the news article to the title of the tab (h1)
+    document.title += " (" + document.getElementsByTagName("h1")[0].innerText + ")";
+} else if (window.location.pathname.includes("/assessments/")) { // check if the current page is an assessments page
+    // change the title of the tab to "Assessments"
+    document.title = "Assessments";
+    // append the title of the assessment to the title of the tab (h1)
+    document.title += " (" + document.getElementsByTagName("h1")[0].innerText + ")";
+} else {
+    // don't change the title of the tab
+}
