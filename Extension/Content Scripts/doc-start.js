cfb3f95e17f9f0730bc5179401765d45ba5ce0bc
42willow <70784752+42Willow@users.noreply.github.com>
2023-05-15 18:33:37 +1000

    Fix missing enabled plugins bug

--- Extension/Content Scripts/doc-start.js
+++ Extension/Content Scripts/doc-start.js
@@ -21,16 +21,26 @@ function injectPlugin(pluginName) {
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             let resp = JSON.parse(xhr.responseText);
-            let scripts = resp[pluginName].scripts;
-            // for each script, inject it
-            // console.groupCollapsed("Plugins (doc-start)");
-            for (let i = 0; i < scripts.length; i++) {
-                if (scripts[i].execute == "doc-start") {
-                    injectJS(scripts[i].path);
-                    // console.log(`Injected Plugin ${i+1}/${scripts.length} (${scripts[i].path})`);
+            // console.log("pluginName", pluginName);
+            // console.log(resp);
+            // console.log(resp[pluginName]);
+            try {
+                let scripts = resp[pluginName].scripts;
+                for (let i = 0; i < scripts.length; i++) {
+                    if (scripts[i].execute == "doc-start") {
+                        injectJS(scripts[i].path);
+                        // console.log(`Injected Plugin ${i+1}/${scripts.length} (${scripts[i].path})`);
+                    }
                 }
+            } catch { // Enabled plugin not found in plugins.json, remove it from the enabled plugins list
+                chrome.storage.local.get(["settings"], function (data) {
+                    let newSettings = data.settings;
+                    newSettings.enabledPlugins.splice(newSettings.enabledPlugins.indexOf(pluginName), 1);
+                    chrome.storage.local.set({settings: newSettings}, function() {
+                        console.log(`Removed ${pluginName} from enabled plugins list`);
+                    });
+                });
             }
-            // console.groupEnd();
         }
     }
     xhr.send();
