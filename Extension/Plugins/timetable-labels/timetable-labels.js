fec1402f984ac870a524df2520af70b2ea4a2868
BC-548 <101848660+BC-548@users.noreply.github.com>
2023-08-08 06:47:13 +1000

    Update timetable-labels.js

--- Extension/Plugins/timetable-labels/timetable-labels.js
+++ Extension/Plugins/timetable-labels/timetable-labels.js
@@ -1,7 +1,9 @@
-document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(2)").innerHTML = "Monday"
-document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(3)").innerHTML = "Tuesday"
-document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(4)").innerHTML = "Wednesday"
-document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(5)").innerHTML = "Thursday"
-document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(6)").innerHTML = "Friday"
-document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(7)").innerHTML = "Saturday"
-document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(8)").innerHTML = "Sunday"
+if (window.location.pathname === "/timetable" && document.getElementsByClassName("timetable")[0]) {
+    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(2)").innerHTML = "Monday"
+    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(3)").innerHTML = "Tuesday"
+    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(4)").innerHTML = "Wednesday"
+    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(5)").innerHTML = "Thursday"
+    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(6)").innerHTML = "Friday"
+    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(7)").innerHTML = "Saturday"
+    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(8)").innerHTML = "Sunday"
+}
