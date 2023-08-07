131a405feb557f9e21e0c284902528c45262448a
BC-548 <101848660+BC-548@users.noreply.github.com>
2023-08-07 22:02:07 +1000

    Add Timetable labels plugin

---
+++ Extension/Plugins/timetable-labels/timetable-labels.js
@@ -0,0 +1,7 @@
+document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(2)").innerHTML = "Monday"
+document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(3)").innerHTML = "Tuesday"
+document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(4)").innerHTML = "Wednesday"
+document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(5)").innerHTML = "Thursday"
+document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(6)").innerHTML = "Friday"
+document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(7)").innerHTML = "Saturday"
+document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(8)").innerHTML = "Sunday"
