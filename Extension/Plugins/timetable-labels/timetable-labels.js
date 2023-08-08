5f5de4ee35512f2155f45396747688e3b475f155
BC-548 <101848660+BC-548@users.noreply.github.com>
2023-08-08 21:27:46 +1000

    Update timetable-labels.js

--- Extension/Plugins/timetable-labels/timetable-labels.js
+++ Extension/Plugins/timetable-labels/timetable-labels.js
@@ -1,9 +1,7 @@
+const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 if (window.location.pathname === "/timetable" && document.getElementsByClassName("timetable")[0]) {
-    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(2)").innerHTML = "Monday"
-    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(3)").innerHTML = "Tuesday"
-    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(4)").innerHTML = "Wednesday"
-    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(5)").innerHTML = "Thursday"
-    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(6)").innerHTML = "Friday"
-    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(7)").innerHTML = "Saturday"
-    document.querySelector("#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(8)").innerHTML = "Sunday"
-}
+    for(let i = 2; i < 9; i++) { // 9-2 = 7 days, as Monday is [2]
+        document.querySelector(`#content > div:nth-child(5) > div:nth-child(2) > div > table > thead > tr > th:nth-child(${i})`).innerHTML = days[i-2]
+        // console.log(i)
+    }
+}   
