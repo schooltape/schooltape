9f01e63cbd0853db46f95143fceb70bb010d0fc1
42Willow <70784752+42Willow@users.noreply.github.com>
2024-02-07 18:08:05 +1100

    fix: schooltape site settings links

--- extension/options/option-links.js
+++ extension/options/option-links.js
@@ -5,7 +5,6 @@ injectLinks(["settings","plugins","themes"]);
 function injectLinks(linkList) {
     for (let i = 0; i < linkList.length; i++) {
         let link = document.getElementsByClassName(linkList[i])[0];
-        let extensionProtocol = getExtensionProtocol();
         link.href = `${browser.runtime.getURL('/')}options/${linkList[i]}.html`
         console.log("Injected link: " + link.href);
     }
