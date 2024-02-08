fd6058219ab9a77b7e6a18a67b57bfbf7285cc62
42Willow <70784752+42Willow@users.noreply.github.com>
2024-02-08 21:23:29 +1100

    refactor: mv2

--- extension/options/option-links.js
+++ extension/options/option-links.js
@@ -5,7 +5,7 @@ injectLinks(["settings","plugins","themes"]);
 function injectLinks(linkList) {
     for (let i = 0; i < linkList.length; i++) {
         let link = document.getElementsByClassName(linkList[i])[0];
-        link.href = `${browser.runtime.getURL('/')}options/${linkList[i]}.html`
+        link.href = `${chrome.runtime.getURL('/')}options/${linkList[i]}.html`
         console.log("Injected link: " + link.href);
     }
 }
