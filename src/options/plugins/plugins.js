7ba9e45186a18ec4bcc0e0ecf7828ba26acbdea6
42Willow <70784752+42Willow@users.noreply.github.com>
2024-02-19 15:45:23 +1100

    feat: plugin toggles actually work

--- src/options/plugins/plugins.js
+++ src/options/plugins/plugins.js
@@ -1,15 +1,13 @@
-injectPlugins();
 
-// Get plugins from plugins.json
-function injectPlugins() {
-    fetch("/plugins/plugins.json")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            addPlugins(data);
-        }
-    );
-}
+
+// inject plugins
+fetch("/plugins/plugins.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        addPlugins(data);
+    }
+);
 
 // Add plugins to the page
 function addPlugins(data) {
@@ -17,42 +15,65 @@ function addPlugins(data) {
         let options = document.querySelector(".plugins-container");
         let plugins = Object.entries(data);
         console.log(settingsData);
-        for (let i = 0; i < plugins.length; i++) {
-            let plugin = plugins[i];
+        plugins.forEach((plugin) => {
+            let pluginId = plugin[0];
+            let pluginName = plugin[1].name;
+            let pluginDescription = plugin[1].description;
+            let pluginToggled = false;
+            // console.log(pluginId);
+            // console.log(settingsData.settings.enabledPlugins);
+            if (settingsData.settings.enabledPlugins.includes(pluginId)) {
+                pluginToggled = true;
+            }
             let option = document.createElement("div");
             option.classList.add("my-4");
             option.classList.add("group");
             option.innerHTML = `
                 <label class="relative flex justify-between items-center group py-2 text-xl text-text">
-                    <h4 class="text-text">${plugin[1].name}</h4>
-                    <input plugin-id="${plugin[0]} id="theme-toggle" type="checkbox" class="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md" />
+                    <h4 class="text-text">${pluginName}</h4>
+                    <input plugin-id="${pluginId}" ${pluginToggled ? 'checked' : ''} type="checkbox" class="plugin-toggle absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md" />
                     <span class="w-11 h-5 flex items-center flex-shrink-0 ml-4 p-1 bg-red rounded-lg duration-500 ease-in-out peer-checked:bg-green after:w-3 after:h-3 after:bg-base after:rounded-lg after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
                 </label>
-                <div class="text-subtext0 group-hover:text-subtext1">${plugin[1].description}</div>
+                <div class="text-subtext0 group-hover:text-subtext1">${pluginDescription}</div>
             `;
             options.appendChild(option);
-        }
+        });
+
+        // toggle listeners
+        const pluginToggles = document.querySelectorAll(".plugin-toggle");
+
+        pluginToggles.forEach((toggle) => {
+            // console.log(toggle);
+            toggle.addEventListener("click", toggleClicked);
+        });
     });
 }
-function installPlugin(button) {
-    let pluginName = button.getAttribute("plugin-name");
+
+function toggleClicked(event) {
+    // console.log(event.target);
+    let pluginId = event.target.getAttribute("plugin-id");
+    if (event.target.checked) {
+        installPlugin(pluginId);
+    } else {
+        uninstallPlugin(pluginId);
+    }
+}
+
+function installPlugin(pluginId) {
     chrome.storage.local.get(['settings'], function(settingsData) {
         let settings = settingsData.settings;
-        settings.enabledPlugins.push(pluginName);
+        settings.enabledPlugins.push(pluginId);
         chrome.storage.local.set({"settings": settings}, function() {
-            console.log(`Installed plugin ${pluginName}`);
+            console.log(`Installed plugin ${pluginId}`);
         });
     });
-    window.location.reload();
 }
-function uninstallPlugin(button) {
-    let pluginName = button.getAttribute("plugin-name");
+function uninstallPlugin(pluginId) {
     chrome.storage.local.get(['settings'], function(settingsData) { 
         let settings = settingsData.settings;
-        settings.enabledPlugins.splice(settings.enabledPlugins.indexOf(pluginName), 1);
+        settings.enabledPlugins.splice(settings.enabledPlugins.indexOf(pluginId), 1);
         chrome.storage.local.set({"settings": settings}, function() {
-            console.log(`Uninstalled plugin ${pluginName}`);
+            console.log(`Uninstalled plugin ${pluginId}`);
         });
     });
-    window.location.reload();
 }
