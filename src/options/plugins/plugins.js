2c3e06de4dda0f722ecab17053e80dbb86a9e49f
42Willow <70784752+42Willow@users.noreply.github.com>
2024-02-19 14:55:48 +1100

    style: remove comments from plugins popup script

--- src/options/plugins/plugins.js
+++ src/options/plugins/plugins.js
@@ -13,7 +13,7 @@ function injectPlugins() {
 
 // Add plugins to the page
 function addPlugins(data) {
-    chrome.storage.local.get(['settings'], function(settingsData) { // get settings
+    chrome.storage.local.get(['settings'], function(settingsData) {
         let options = document.querySelector(".plugins-container");
         let plugins = Object.entries(data);
         console.log(settingsData);
@@ -22,34 +22,6 @@ function addPlugins(data) {
             let option = document.createElement("div");
             option.classList.add("my-4");
             option.classList.add("group");
-            // Check if plugin is installed (chrome storage)
-            // if (settingsData.settings.enabledPlugins.includes(plugin[0])) {
-            //     option.innerHTML = `
-            //         <label class="relative flex justify-between items-center group p-2 text-xl text-text">
-            //             <h2 class="from-blue to-teal">Themes</h2>
-            //             <input id="theme-toggle" type="checkbox" class="absolute left-1/2 -translate-x-1/2 w-full h-full peer appearance-none rounded-md" />
-            //             <span class="w-14 h-8 flex items-center flex-shrink-0 ml-4 p-1 bg-red rounded-lg duration-500 ease-in-out peer-checked:bg-green after:w-6 after:h-6 after:bg-base after:rounded-lg after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
-            //         </label>
-            //         <div class="text-subtext0">${plugin[1].description}</div>
-            //         <button class="small uninstall-plugin bg-green" plugin-name="${plugin[0]}">Enabled</button>
-            //     `;
-            //     // add listener for button
-            //     let uninstallButton = option.querySelector(".uninstall-plugin");
-            //     uninstallButton.addEventListener("click", () => {
-            //         uninstallPlugin(uninstallButton);
-            //     });
-            // } else {
-            //     option.innerHTML = `
-            //         <h3 class="text-text mt-4">${plugin[1].name}</h3>
-            //         <div class="text-subtext0">${plugin[1].description}</div>
-            //         <button class="small install-plugin bg-red" plugin-name="${plugin[0]}">Disabled</button>
-            //     `;
-            //     // add listener for button
-            //     let installButton = option.querySelector(".install-plugin");
-            //     installButton.addEventListener("click", () => {
-            //         installPlugin(installButton);
-            //     });
-            // }
             option.innerHTML = `
                 <label class="relative flex justify-between items-center group py-2 text-xl text-text">
                     <h4 class="text-text">${plugin[1].name}</h4>
@@ -64,7 +36,7 @@ function addPlugins(data) {
 }
 function installPlugin(button) {
     let pluginName = button.getAttribute("plugin-name");
-    chrome.storage.local.get(['settings'], function(settingsData) { // get settings
+    chrome.storage.local.get(['settings'], function(settingsData) {
         let settings = settingsData.settings;
         settings.enabledPlugins.push(pluginName);
         chrome.storage.local.set({"settings": settings}, function() {
@@ -75,7 +47,7 @@ function installPlugin(button) {
 }
 function uninstallPlugin(button) {
     let pluginName = button.getAttribute("plugin-name");
-    chrome.storage.local.get(['settings'], function(settingsData) { // get settings
+    chrome.storage.local.get(['settings'], function(settingsData) { 
         let settings = settingsData.settings;
         settings.enabledPlugins.splice(settings.enabledPlugins.indexOf(pluginName), 1);
         chrome.storage.local.set({"settings": settings}, function() {
