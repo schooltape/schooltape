c582a4aea6f7ab6e9242207c64bc6b7798ac2ea6
Willow <42willow@pm.me>
2024-05-15 19:38:37 +1000

    refactor: styling fixes

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -1,6 +1,3 @@
-console.log("eqwe");
-
-
 let sections = [];
 chrome.storage.local.get(["settings"], function (data) {
     if (data.settings.themes) {
@@ -9,7 +6,7 @@ chrome.storage.local.get(["settings"], function (data) {
         // now we have to split this into three sections, separated by the -'s
         sections = theme.split('-');
         // sections will be an array containing ["catppuccin", "macchiato", "pink"]
-        console.log(sections);
+        // console.log(sections);
         if (sections[0] == "catppuccin") {
             injectCatppuccin(sections[1], sections[2]);
             injectCSS(`/themes/catppuccin.css`);
@@ -19,21 +16,21 @@ chrome.storage.local.get(["settings"], function (data) {
 
 
 function injectCatppuccin(req_flavor, req_accent) {
-    console.log("match!");
+    // console.log("match!");
     // get info from catppuccin.json
     fetch(chrome.runtime.getURL("/themes/catppuccin.json"))
         .then(response => response.json())
         .then(palette => {
-            console.log(palette);
+            // console.log(palette);
             for (let flavor in palette) {
-                console.log(palette[flavor]);
+                // console.log(palette[flavor]);
                 // if flavor is in sections, inject the css vars
                 if (flavor == req_flavor) {
-                    console.log("flavor match!"+flavor);
+                    // console.log("flavor match!"+flavor);
                     for (let color in palette[flavor]["colors"]) {
                         let c = palette[flavor]["colors"][color]
-                        console.log(color);
-                        console.log(c);
+                        // console.log(color);
+                        // console.log(c);
                         document.querySelector(':root').style.setProperty(`--ctp-${color}`, `${c.hsl.h} ${c.hsl.s*100}% ${c.hsl.l*100}%`);
                         if (color === req_accent) {
                             document.querySelector(':root').style.setProperty("--ctp-accent", `${c.hsl.h} ${c.hsl.s*100}% ${c.hsl.l*100}%`);
