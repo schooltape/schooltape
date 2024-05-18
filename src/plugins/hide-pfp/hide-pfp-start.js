19bc42a58a2e1c13de73e9f91583ff3eebc8879b
Willow <42willow@pm.me>
2024-05-18 21:36:01 +1000

    revert: remove hide-pfp plugin (replaced by snippet)

--- src/plugins/hide-pfp/hide-pfp-start.js
+++
@@ -1,5 +0,0 @@
-let css = document.createElement("link");
-css.rel = "stylesheet";
-css.href = chrome.runtime.getURL("/plugins/hide-pfp/hide-pfp.css");
-css.classList.add("profile-picture", "schooltape");
-document.head.appendChild(css);
