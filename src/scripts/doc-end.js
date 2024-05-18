2f5ff2ffe05bec691fab5cc1199f6e05f567ea69
Willow <42willow@pm.me>
2024-05-18 21:31:43 +1000

    refactor: `doc-end.js` #44

--- src/scripts/doc-end.js
+++ src/scripts/doc-end.js
@@ -13,59 +13,20 @@ chrome.storage.local.get(["settings"], function (data) {
 });
 
 // check if the page is Schoolbox, if not add it to storage and reload the page
-schoolboxChecker();
-
-// ----------------- Functions ----------------- //
-
-
-function schoolboxChecker() {
-    // check if the page is schoolbox
-    try {
-        let footer = getChildNode(document.getElementById("footer"), 1);
-        if (footer.innerHTML.includes("Schoolbox")) {
-            let footerListItem = document.createElement("LI");
-            footerListItem.innerHTML = "<a href='https://github.com/42willow/schooltape'>Schooltape</a> "+chrome.runtime.getManifest().version;
-            footer.appendChild(footerListItem);
-            // check if the current url is saved in the storage
-            chrome.storage.local.get("settings", function (data) {
-                // if the url is not saved
-                if (!data.settings.urls.includes(window.location.origin)) {
-                    let settings = data.settings;
-                    settings.urls.push(window.location.origin);
-                    chrome.storage.local.set({"settings": settings}, function () {
-                        // console.log(`%c[doc-end.js]`, docEndConsole, "Added URL to storage");
-                    });
-                    // reload page
-                    window.location.reload();
-                }
-            });
-            return true;
-        } else {
-            return false;
+let footer = document.querySelector("#footer > ul");
+if (footer.innerHTML.includes("Schoolbox")) {
+    let footerListItem = document.createElement("li");
+    footerListItem.appendChild(document.createElement('a')).href = 'https://github.com/42willow/schooltape';
+    footerListItem.firstChild.textContent = `Schooltape v${chrome.runtime.getManifest().version}`;
+    footer.appendChild(footerListItem);
+    chrome.storage.local.get("settings", function ({settings}) {
+        if (!settings.urls.includes(window.location.origin)) {
+            settings.urls.push(window.location.origin);
+            chrome.storage.local.set({"settings": settings});
+            // TODO: hot reload
+            window.location.reload();
         }
-    }
-    catch (e) {
-        return false;
-    }
-}
-
-function getChildNode(node, childNum, nodeName = null) {
-    let childCounter = 0;
-    for (let i = 0; i < node.childNodes.length; i++) {
-        // If node type is an element node
-        if (node.childNodes[i].nodeType === 1) {
-            if (nodeName) {
-                if (node.childNodes[i].nodeName === nodeName) {
-                    childCounter++;
-                }
-            } else {
-                childCounter++;
-            }
-            if (childCounter === childNum) {
-                return node.childNodes[i];
-            }
-        }
-    }
+    });
 }
 
 async function runUtilsFunction(functionName, ...args) {
