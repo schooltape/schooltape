3fd48427705a72998f14ee123d995f3c0eff9461
Willow <42willow@pm.me>
2024-05-20 18:05:28 +1000

    fix: timetable labels sometimes not injecting
    - use MutationObserver

--- src/plugins/timetable-labels/timetable-labels.js
+++ src/plugins/timetable-labels/timetable-labels.js
@@ -1,7 +1,19 @@
-const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-if (window.location.pathname === "/timetable" && document.getElementsByClassName("timetable")[0]) {
-    for(let i = 0; i < 7; i++) {
-        document.querySelector(`.timetable > thead > tr > th:nth-child(${i+2})`).innerHTML = days[i]
-        document.querySelector(`.timetable-small > div:nth-child(${i+1}) > h2`).innerHTML = days[i]
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+const observer = new MutationObserver((mutationsList, observer) => {
+    for(let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            const timetable = document.querySelector(".timetable");
+            const timetableSmall = document.querySelector(".timetable-small");
+            if (timetable && timetableSmall) {
+                observer.disconnect();
+                days.forEach((day, i) => {
+                    document.querySelector(`.timetable > thead > tr > th:nth-child(${i+2})`).innerHTML = day;
+                    document.querySelector(`.timetable-small > div:nth-child(${i+1}) > h2`).innerHTML = day;
+                });
+                return;
+            }
+        }
     }
-}
+});
+observer.observe(document, { childList: true, subtree: true });
