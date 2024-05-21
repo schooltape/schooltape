083e4363192895ebe99d51cd80e1ad14c3f36c1b
Willow <42willow@pm.me>
2024-05-21 21:40:23 +1000

    style: move return from block

--- src/plugins/homepage-switcher/homepage-switcher.js
+++ src/plugins/homepage-switcher/homepage-switcher.js
@@ -2,9 +2,7 @@ let logos = Array.from(document.getElementsByClassName("logo"));
 
 logos.forEach((logo) => {
   logo.addEventListener("click", function (e) {
-    if (window.location.pathname === "/") {
-      return;
-    }
+    if (window.location.pathname === "/") return;
     e.preventDefault();
     chrome.storage.local.get(["settings"], function () {
       let tab = logos[0].href;
