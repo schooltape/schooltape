15098b0a6cd971d1cec8b2bfbe672504e517d2ad
Willow <42willow@pm.me>
2024-05-21 21:24:18 +1000

    refactor: tab-title.js #57

--- src/plugins/tab-title/tab-title.js
+++ src/plugins/tab-title/tab-title.js
@@ -3,32 +3,37 @@ try {
     document.title = document.getElementById("heading").getAttribute("value");
   }
 } catch (error) {}
-if (window.location.pathname === "/") {
-  document.title = "Homepage";
-} else if (window.location.pathname === "/timetable") {
-  document.title = "Timetable";
-} else if (window.location.pathname.includes("/calendar")) {
+
+const path = window.location.pathname;
+const titleMap = {
+  "/": "Homepage",
+  "/timetable": "Timetable",
+  "/calendar": "Calendar",
+  "/news": "News",
+  "/learning/classes": "Classes",
+  "/resources": "Resources",
+  "/settings/messages": "Message Settings",
+  "/mail/create": "Compose Email",
+  "/feedback": "Support and Feedback",
+  "/policy": "Guidelines of Use and Privacy Policy",
+};
+
+if (titleMap.hasOwnProperty(path)) {
+  document.title = titleMap[path];
+} else if (path.includes("/calendar")) {
   document.title = "Calendar";
-} else if (window.location.pathname === "/news") {
-  document.title = "News";
-} else if (window.location.pathname === "/learning/classes") {
-  document.title = "Classes";
-} else if (window.location.pathname.includes("/grades/")) {
+} else if (path.includes("/grades/")) {
   document.title = "Grades";
-} else if (window.location.pathname.includes("/news/")) {
+} else if (path.includes("/news/")) {
   document.title = `News (${document.getElementsByTagName("h1")[0].innerText})`;
-} else if (window.location.pathname.includes("/assessments/")) {
-  document.title = `Assessments (${document.getElementsByTagName("h1")[0].innerText})`;
-} else if (window.location.pathname === "/resources") {
-  document.title = "Resources";
-} else if (window.location.pathname === "/settings/messages") {
-  document.title = "Message Settings";
-} else if (window.location.pathname.includes("/mail/create")) {
+} else if (path.includes("/assessments/")) {
+  document.title = `Assessments - ${document.getElementsByTagName("h1")[0].innerText})`;
+} else if (path.includes("/mail/create")) {
   document.title = "Compose Email";
-} else if (window.location.pathname.includes("/search/user")) {
-  document.title = `User Profile - ${document.getElementsByTagName("h1")[0].innerText}`;
-} else if (window.location.pathname === "/feedback") {
-  document.title = "Support and Feedback";
-} else if (window.location.pathname === "/policy") {
-  document.title = "Guidelines of Use and Privacy Policy";
+} else if (path.includes("/search/user")) {
+  document.title = `Profile - ${document.getElementsByTagName("h1")[0].innerText}`;
+} else if (path.includes("/learning/due/")) {
+  document.title = "Due Work";
+} else if (path.includes("/homepage/")) {
+  document.title = document.getElementsByTagName("h1")[0].innerText;
 }
