a102c943c5775aa3f7ccd089a9c30ca0afa7de0f
Willow <42willow@pm.me>
2024-05-25 21:10:35 +1000

    refactor: finally finish refactoring scroll-period and subheader
    - note: there is duplicated code as the asynchronous module approach wasn't working

--- src/plugins/subheader/subheader.js
+++ src/plugins/subheader/subheader.js
@@ -32,7 +32,7 @@ if (window.location.pathname === "/" && document.getElementsByClassName("timetab
   setInterval(updateDateSpan, 60000);
 }
 
-async function createSubheader() {
+function createSubheader() {
   const subheader = document.querySelector("h2.subheader");
   // TODO: Refactor to support hot reload/uninjection
   // delete all children of the subheader
@@ -48,7 +48,7 @@ async function createSubheader() {
   updateDateSpan()
 }
 
-async function updatePeriodSpan() {
+function updatePeriodSpan() {
   let periodSpan = document.querySelector(".subheader .period");
   if (!periodSpan) {
     const subheader = document.querySelector(".subheader .schooltape");
@@ -57,7 +57,7 @@ async function updatePeriodSpan() {
     subheader.appendChild(periodSpan);
   }
 
-  let period = await runUtilsFunction("getCurrentPeriod");
+  let period = getCurrentPeriod()
   if (period) {
     const name = period.data.name || period.header.name;
     const room = period.data.room ? ` (${period.data.room})` : "";
@@ -106,12 +106,88 @@ function updateDateSpan() {
   dateSpan.textContent = date.toDateString();
 }
 
-async function runUtilsFunction(functionName, ...args) {
-  const src = chrome.runtime.getURL("plugins/pluginUtils.js");
-  const utils = await import(src);
-  if (typeof utils[functionName] === "function") {
-    return utils[functionName](...args);
-  } else {
-    console.error(`Function ${functionName} does not exist in utils`);
+// Period Utils (content scripts do not have support for modules)
+
+function getListOfPeriods() {
+  const periods = document.querySelectorAll(".timetable thead tr th");
+  return Array.from(periods).map((_, i) => getPeriodData(i));
+}
+
+function getCurrentPeriod() {
+  const periodList = getListOfPeriods();
+  const currentTime = new Date().getTime();
+  const currentPeriod = periodList.find((period) => {
+    if (period.header.time) {
+      const { start, end } = period.header.time;
+      // console.log("start time: ", start.getTime(), "end time: ", end.getTime()); // log the start and end times
+      if (start.getTime() <= currentTime && currentTime <= end.getTime()) {
+        return true;
+      }
+    }
+    return false;
+  });
+  // console.log("currentPeriod: ", currentPeriod); // log the currentPeriod
+  return currentPeriod || null;
+}
+
+function getPeriodData(index) {
+  if (typeof index !== "number") {
+    console.error("Period number was not provided or is not a number");
+    return null;
+  }
+
+  index += 1;
+  // Example data structure:
+  // {
+  //   header: {
+  //     name: "Period 1"
+  //     time: "8:30am-8-44am"
+  //   },
+  //   data: {
+  //     name: "Programming - Schooltape"
+  //     link: "/home/page/code/schooltape"
+  //     id: "(-ST-CC)"
+  //     room: "Library"
+  //   }
+  // }
+  const header = document.querySelector(`.timetable thead tr th:nth-child(${index})`);
+  const data = document.querySelector(`.timetable tbody tr td:nth-child(${index}) div:nth-child(1) div:nth-child(1)`);
+
+  // console.log(header, data);
+
+  return {
+    header: {
+      name: header?.childNodes[0]?.textContent.trim() || null,
+      time: extractTimes(header?.querySelector("time")?.textContent.trim()) || null,
+    },
+    data: {
+      name: data?.querySelector("a")?.textContent.trim() || null,
+      link: data?.querySelector("a")?.getAttribute("href") || null,
+      id: data?.querySelector("div:nth-child(2)")?.textContent.trim() || null,
+      room: data?.querySelector("div:nth-child(3)")?.textContent.trim() || null,
+    },
+    index: index,
+  };
+}
+
+function extractTimes(periodTime) {
+  try {
+    let times = periodTime.split("–"); // en dash
+    let [start, end] = times.map((time) => {
+      let [hour, minute] = time.split(":");
+      let isAM = time.slice(-2) === "am";
+      hour = parseInt(hour);
+      minute = parseInt(minute.substring(0, 2));
+      if (!isAM && hour !== 12) {
+        hour += 12;
+      }
+      let date = new Date();
+      date.setHours(hour, minute, 0, 0);
+      return date;
+    });
+    return { start, end };
+  } catch (error) {
+    console.error(error);
+    return false;
   }
 }
