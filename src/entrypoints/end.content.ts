2df62f6c06f3c58f1068ec3095bac394f593dc77
Willow <42willow@pm.me>
2024-06-24 16:35:49 +1000

    fix: end content script crashing on startup
    - on all non-Schoolbox pages

--- src/entrypoints/end.content.ts
+++ src/entrypoints/end.content.ts
@@ -4,7 +4,7 @@ export default defineContentScript({
   excludeMatches: ["*://*/learning/quiz/*"],
   async main() {
     let settings = await globalSettings.getValue();
-    let plugins = await pluginSettings.getValue();
+    // let plugins = await pluginSettings.getValue();
 
     if (settings.global) {
       if (settings.urls.includes(window.location.origin)) {
@@ -17,10 +17,12 @@ export default defineContentScript({
       } else {
         // detect Schoolbox
         let footer = document.querySelector("#footer > ul");
-        if (footer.innerHTML.includes("Schoolbox")) {
+        if (footer && footer.innerHTML.includes("Schoolbox")) {
           let footerListItem = document.createElement("li");
-          footerListItem.appendChild(document.createElement("a")).href = "https://github.com/schooltape/schooltape";
-          footerListItem.firstChild.textContent = `Schooltape v${browser.runtime.getManifest().version}`;
+          let footerLink = document.createElement("a");
+          footerLink.href = "https://github.com/schooltape/schooltape";
+          footerLink.textContent = `Schooltape v${browser.runtime.getManifest().version}`;
+          footerListItem.appendChild(footerLink);
           footer.appendChild(footerListItem);
           if (!settings.urls.includes(window.location.origin)) {
             settings.urls.push(window.location.origin);
