11cfc1c044de1a4c1d9bfa1d13a950caa23ace13
DragonIsGone <rtclemow@icloud.com>
2024-07-31 11:48:42 +1000

    fix: timetableLabels not injecting

--- src/modules/plugins/timetableLabels.js
+++ src/modules/plugins/timetableLabels.js
@@ -1,25 +1,14 @@
 export default defineWxtPlugin(() => {
   defineStPlugin("timetableLabels", () => {
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-    if (window.location.pathname === "/timetable" && document.getElementsByClassName("timetable")[0]) {
-      const observer = new MutationObserver((mutationsList, observer) => {
-        for (let mutation of mutationsList) {
-          if (mutation.type === "childList") {
-            const timetable = document.querySelector(".timetable");
-            const timetableSmall = document.querySelector(".timetable-small");
-            if (timetable && timetableSmall) {
-              observer.disconnect();
-              days.forEach((day, i) => {
-                document.querySelector(`.timetable > thead > tr > th:nth-child(${i + 2})`).innerHTML = day;
-                document.querySelector(`.timetable-small > div:nth-child(${i + 1}) > h2`).innerHTML = day;
-              });
-              return;
-            }
+    function inject() {
+      const days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+      if (window.location.pathname === "/timetable" && document.getElementsByClassName("timetable")[0]) {
+          for(let i = 0; i < 7; i++) {
+              document.querySelector(`.timetable > thead > tr > th:nth-child(${i+2})`).innerHTML = days[i]
+              document.querySelector(`.timetable-small > div:nth-child(${i+1}) > h2`).innerHTML = days[i]
           }
-        }
-      });
-      observer.observe(document, { childList: true, subtree: true });
+      }
     }
+    setTimeout(inject, 500);
   });
 });
