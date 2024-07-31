8a521049e022c853b6a4e042cfaeb380fbfd3dc9
Willow <42willow@pm.me>
2024-07-31 21:49:11 +1000

    feat(smartLinks): functionality

--- src/modules/plugins/smartLinks.ts
+++ src/modules/plugins/smartLinks.ts
@@ -1,30 +1,23 @@
-import { forEachChild } from "typescript";
-
 export default async function defineWxtPlugin() {
   defineStPlugin("smartLinks", () => {
-    // Select all relevant links and convert to an array of HTMLAnchorElement
-    let logos = Array.from(document.getElementsByClassName("logo")) as HTMLAnchorElement[];
-    let timetableLinks = Array.from(document.querySelectorAll(".timetable-subject > a")) as HTMLAnchorElement[];
-    let topLinks = Array.from(document.querySelectorAll(".top-menu > li > a")) as HTMLAnchorElement[];
-    let sideLinks = Array.from(document.querySelectorAll(".second-nav > li > a")) as HTMLAnchorElement[];
-    let vCardLinks = Array.from(document.querySelectorAll(".v-card > a")) as HTMLAnchorElement[];
-    let tileLinks = Array.from(document.querySelectorAll(".tile > a")) as HTMLAnchorElement[];
-    let breadLinks = Array.from(document.querySelectorAll(".breadcrumbs > li > a")) as HTMLAnchorElement[];
-    let infoLinks = Array.from(document.querySelectorAll(".list-item > div > h3 > a")) as HTMLAnchorElement[];
+    let links = document.querySelectorAll("a");
 
-    // Combine all links into one array
-    let allLinks = [...logos, ...timetableLinks, ...topLinks, ...sideLinks, ...vCardLinks, ...tileLinks, ...breadLinks, ...infoLinks];
-
-    // Define a common click event handler
-    function handleClick(e: Event) {
+    // click event handler
+    async function handleClick(e: Event) {
       e.preventDefault();
       let target = e.currentTarget as HTMLAnchorElement;
       let tab = target.href;
+      if (!tab) return;
+      // check if link is a Schoolbox URL
+      let schoolboxURLs = (await globalSettings.getValue()).urls;
+      if (!schoolboxURLs.some((url) => tab.startsWith(url))) {
+        // do default action if not a Schoolbox URL
+        return window.open(tab, target.target);
+      }
       browser.runtime.sendMessage({ toTab: tab });
     }
 
-    // Add click event listener to all links
-    allLinks.forEach((link) => {
+    links.forEach((link) => {
       link.addEventListener("click", handleClick);
     });
   });
