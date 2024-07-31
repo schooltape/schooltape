f392ef6c9c14c41445bc827353b17df76061b2ac
Willow <42willow@pm.me>
2024-07-31 20:59:40 +1000

    fix: schooltape not injecting into footer

--- src/entrypoints/end.content.ts
+++ src/entrypoints/end.content.ts
@@ -6,30 +6,23 @@ export default defineContentScript({
     let settings = await globalSettings.getValue();
     // let plugins = await pluginSettings.getValue();
 
-    if (settings.global) {
-      if (settings.urls.includes(window.location.origin)) {
-        // inject plugins
-        // if (plugins.toggle) {
-        //   for (let i = 0; i < plugins.enabled.length; i++) {
-        //     injectPlugin(plugins.enabled[i], "doc-end");
-        //   }
-        // }
-      } else {
-        // detect Schoolbox
-        let footer = document.querySelector("#footer > ul");
-        if (footer && footer.innerHTML.includes("Schoolbox")) {
-          let footerListItem = document.createElement("li");
-          let footerLink = document.createElement("a");
-          footerLink.href = "https://github.com/schooltape/schooltape";
-          footerLink.textContent = `Schooltape v${browser.runtime.getManifest().version}`;
-          footerListItem.appendChild(footerLink);
-          footer.appendChild(footerListItem);
-          if (!settings.urls.includes(window.location.origin)) {
-            settings.urls.push(window.location.origin);
-            await globalSettings.setValue(settings);
-            // TODO: hot reload
-            window.location.reload();
-          }
+    if (!settings.global) return;
+    let footer = document.querySelector("#footer > ul");
+    if (footer && footer.innerHTML.includes("Schoolbox")) {
+      let footerListItem = document.createElement("li");
+      let footerLink = document.createElement("a");
+      footerLink.href = "https://github.com/schooltape/schooltape";
+      footerLink.textContent = `Schooltape v${browser.runtime.getManifest().version}`;
+      footerListItem.appendChild(footerLink);
+      footer.appendChild(footerListItem);
+
+      if (!settings.urls.includes(window.location.origin)) {
+        logger.info("[end.content.ts] URL not in settings, adding...");
+        if (!settings.urls.includes(window.location.origin)) {
+          settings.urls.push(window.location.origin);
+          await globalSettings.setValue(settings);
+          // TODO: hot reload
+          window.location.reload();
         }
       }
     }
