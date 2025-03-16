c2314b661ef07808d2be4f00cc2e492d92086a2f
willow <42willow@pm.me>
2025-03-16 20:58:04 +1100

    refactor: prepare for v4

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "wxt";
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -17,4 +18,7 @@ export default defineConfig({
   srcDir: "src",
   outDir: "dist",
   modules: ["@wxt-dev/module-svelte"],
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
 });
