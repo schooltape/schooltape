bcd8a54bf16cf029a16a9597aa4dbea26d0d61b5
willow <42willow@pm.me>
2025-05-27 12:50:36 +1000

    fix: plugins and snippets

--- src/utils/plugin.ts
+++ src/utils/plugin.ts
@@ -5,7 +5,7 @@ export async function defineStPlugin(
 ) {
   const plugin = await plugins[pluginId].storage.getValue();
 
-  logger.info(`${PLUGIN_INFO[pluginId].name}: ${plugin.toggle ? "enabled" : "disabled"}`);
+  logger.info(`${plugins[pluginId].info?.name}: ${plugin.toggle ? "enabled" : "disabled"}`);
 
   const settings = await globalSettings.storage.getValue();
   const urls = await schoolboxUrls.storage.getValue();
@@ -19,7 +19,7 @@ export async function defineStPlugin(
             const allElementsPresent = elementsToWaitFor.every((selector) => document.querySelector(selector) !== null);
             if (allElementsPresent) {
               observer.disconnect();
-              logger.info(`all elements present, injecting plugin: ${PLUGIN_INFO[pluginId].name}`);
+              logger.info(`all elements present, injecting plugin: ${plugins[pluginId].info?.name}`);
               injectLogic(pluginId);
             }
           });
@@ -30,12 +30,12 @@ export async function defineStPlugin(
           const allElementsPresent = elementsToWaitFor.every((selector) => document.querySelector(selector) !== null);
           if (allElementsPresent) {
             observer.disconnect();
-            logger.info(`all elements already present, injecting plugin: ${PLUGIN_INFO[pluginId].name}`);
+            logger.info(`all elements already present, injecting plugin: ${plugins[pluginId].info?.name}`);
             injectLogic(pluginId);
           }
         } else {
           // no elements to wait for
-          logger.info(`injecting plugin: ${PLUGIN_INFO[pluginId].name}`);
+          logger.info(`injecting plugin: ${plugins[pluginId].info?.name}`);
           injectLogic(pluginId);
         }
       };
--- src/utils/snippet.ts
+++ src/utils/snippet.ts
@@ -1,7 +1,7 @@
 export async function defineStSnippet(snippetId: SnippetId, styleText: string) {
   const snippet = await snippets[snippetId].storage.getValue();
 
-  logger.info(`${SNIPPET_INFO[snippetId].name}: ${snippet.toggle ? "enabled" : "disabled"}`);
+  logger.info(`${snippets[snippetId].info?.name}: ${snippet.toggle ? "enabled" : "disabled"}`);
 
   const settings = await globalSettings.storage.getValue();
   const urls = await schoolboxUrls.storage.getValue();
@@ -9,7 +9,7 @@ export async function defineStSnippet(snippetId: SnippetId, styleText: string) {
   if (snippet && typeof window !== "undefined" && urls.includes(window.location.origin)) {
     if (settings.global && settings.snippets && snippet.toggle) {
       // inject
-      logger.info(`Injecting snippet: ${SNIPPET_INFO[snippetId].name}`);
+      logger.info(`Injecting snippet: ${snippets[snippetId].info?.name}`);
       injectStyles(styleText);
     }
   }
