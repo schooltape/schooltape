c7ed1d745326f66d86f0d7ed1b74400cd562fa71
willow <42willow@pm.me>
2025-12-01 21:23:50 +1100

    feat: partial user snippet and theme hot reload

--- src/entrypoints/start.content.ts
+++ src/entrypoints/start.content.ts
@@ -1,9 +1,18 @@
 import { browser, defineContentScript } from "#imports";
-import { injectCatppuccin, injectLogo, injectStylesheet, injectUserSnippets } from "@/utils";
+import {
+  injectCatppuccin,
+  injectLogo,
+  injectStylesheet,
+  uninjectStylesheet,
+  injectUserSnippet,
+  uninjectUserSnippet,
+  hasChanged,
+} from "@/utils";
 import { EXCLUDE_MATCHES, LOGO_INFO } from "@/utils/constants";
-import type { LogoId } from "@/utils/storage";
+import type { LogoId, Settings } from "@/utils/storage";
 import { globalSettings, schoolboxUrls } from "@/utils/storage";
 import cssUrl from "./catppuccin.css?url";
+import { WatchCallback } from "wxt/utils/storage";
 
 export default defineContentScript({
   matches: ["<all_urls>"],
@@ -14,23 +23,62 @@ export default defineContentScript({
     const settings = await globalSettings.storage.getValue();
     const urls = (await schoolboxUrls.storage.getValue()).urls;
 
+    const updateThemes: WatchCallback<Settings> = (newValue, oldValue) => {
+      // if global or themes was changed
+      if (hasChanged(newValue, oldValue, ["global", "themes"])) {
+        if (newValue.global && newValue.themes) {
+          injectThemes();
+        } else {
+          uninjectThemes();
+        }
+      }
+    };
+
+    const updateUserSnippets: WatchCallback<Settings> = (newValue, oldValue) => {
+      // if global or userSnippets were changed
+      if (hasChanged(newValue, oldValue, ["global", "userSnippets"])) {
+        for (const [id, userSnippet] of Object.entries(newValue.userSnippets)) {
+          if (newValue.global && newValue.snippets && userSnippet.toggle) {
+            injectUserSnippet(id);
+          } else {
+            uninjectUserSnippet(id);
+          }
+        }
+      }
+    };
+
+    const injectThemes = () => injectStylesheet(cssUrl, "themes");
+    const uninjectThemes = () => uninjectStylesheet("themes");
+
     if (settings.global && urls.includes(window.location.origin)) {
       // inject themes
       if (settings.themes) {
-        injectStylesheet(cssUrl);
+        injectThemes();
+
+        // inject CSS variables
         injectCatppuccin(settings.themeFlavour, settings.themeAccent);
       }
 
       // inject logo
       injectLogo(LOGO_INFO[settings.themeLogo as LogoId], settings.themeLogoAsFavicon);
 
-      // inject snippets
+      // inject user snippets
       if (settings.snippets) {
-        injectUserSnippets(settings.userSnippets);
+        const userSnippets = globalSettings.get().userSnippets;
+        for (const id of Object.keys(userSnippets)) {
+          injectUserSnippet(id);
+        }
       }
 
       // update icon
       browser.runtime.sendMessage({ updateIcon: true });
+
+      // storage listeners for hot reload
+      // if global is toggled, inject or uninject theme and snippets
+      globalSettings.storage.watch((newValue, oldValue) => {
+        updateThemes(newValue, oldValue);
+        updateUserSnippets(newValue, oldValue);
+      });
     }
   },
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,12 @@
 import { browser } from "#imports";
 import { flavorEntries } from "@catppuccin/palette";
 import { logger } from "./logger";
-import type { LogoInfo, UserSnippet } from "./storage";
+import { globalSettings } from "./storage";
+import type { LogoInfo } from "./storage";
 
+// TODO: uninjectStyles
 export function injectStyles(styleText: string) {
-  logger.info(`[content-utils] Injecting styles`);
+  logger.info(`injecting styles`);
   const style = document.createElement("style");
   style.textContent = styleText;
   style.classList.add("schooltape");
@@ -12,7 +14,7 @@ export function injectStyles(styleText: string) {
 }
 
 export function injectCatppuccin(flavour: string, accent: string) {
-  logger.info(`[content-utils] Injecting Catppuccin: ${flavour} ${accent}`);
+  logger.info(`injecting catppuccin: ${flavour} ${accent}`);
   let styleText = ":root {";
   const flavourArray = flavorEntries.find((entry) => entry[0] === flavour);
   if (flavourArray) {
@@ -33,7 +35,7 @@ export function injectLogo(logo: LogoInfo, setAsFavicon: boolean) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     url = browser.runtime.getURL(url as any);
   }
-  logger.info(`[content-utils] Injecting Logo: ${logo.name}`);
+  logger.info(`injecting logo: ${logo.name}`);
   if (logo.disable) {
     return;
   }
@@ -72,29 +74,93 @@ export function injectLogo(logo: LogoInfo, setAsFavicon: boolean) {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function injectStylesheet(url: any) {
-  logger.info(`[content-utils] Injecting stylesheet: ${url}`);
+export function injectStylesheet(url: any, id: string) {
+  // check if stylesheet has already been injected
+  const existingLink = document.querySelector(`link[data-schooltape="${id}"]`);
+  if (existingLink) {
+    logger.info(`stylesheet with id ${id} already injected, aborting`);
+    return;
+  }
+
+  // inject stylesheet
+  logger.info(`injecting stylesheet with id ${id}: ${url}`);
   const link = document.createElement("link");
   link.rel = "stylesheet";
   link.href = browser.runtime.getURL(url);
-  link.classList.add("schooltape");
+  link.dataset.schooltape = id;
   document.head.appendChild(link);
 }
 
-export async function injectUserSnippets(userSnippets: Record<string, UserSnippet>) {
-  logger.info("[content-utils] Injecting snippets");
-  // user snippets
-  Object.keys(userSnippets).forEach((snippetId) => {
-    const userSnippet = userSnippets[snippetId];
-    if (userSnippet.toggle) {
-      fetch(`https://gist.githubusercontent.com/${userSnippet.author}/${snippetId}/raw`)
-        .then((response) => response.text())
-        .then((css) => {
-          const style = document.createElement("style");
-          style.textContent = css;
-          style.classList.add("schooltape");
-          document.head.appendChild(style);
-        });
+export function uninjectStylesheet(id: string) {
+  logger.info(`uninjecting stylesheet with id ${id}`);
+  const link = document.querySelector(`link[data-schooltape="${id}"]`);
+  if (link) {
+    document.head.removeChild(link);
+  } else {
+    // logger.warn(`stylesheet with id ${id} not found, aborting`);
+  }
+}
+
+export function injectUserSnippet(id: string) {
+  logger.info(`injecting user snippet with id ${id}`);
+
+  const userSnippets = globalSettings.get().userSnippets;
+  const snippet = userSnippets[id];
+
+  if (!snippet) {
+    logger.error(`user snippet with id ${id} not found, aborting`);
+    return;
+  }
+
+  if (snippet.toggle === true) {
+    // check not already injected
+    const style = document.querySelector(`style[data-schooltape="userSnippet-${id}"]`);
+    if (style) {
+      logger.info(`user snippet with id ${id} already injected, aborting`);
+      return;
     }
-  });
+
+    // inject user snippet
+    fetch(`https://gist.githubusercontent.com/${snippet.author}/${id}/raw`)
+      .then((response) => response.text())
+      .then((css) => {
+        const style = document.createElement("style");
+        style.textContent = css;
+        style.dataset.schooltape = `userSnippet-${id}`;
+        document.head.appendChild(style);
+        logger.info(`injected user snippet with id ${id}`);
+      });
+  }
+}
+
+export function uninjectUserSnippet(id: string) {
+  logger.info(`uninjecting user snippet with id ${id}`);
+  const style = document.querySelector(`style[data-schooltape="userSnippet-${id}"]`);
+  if (style) {
+    document.head.removeChild(style);
+    logger.info(`uninjected user snippet with id ${id}`);
+  } else {
+    // logger.warn(`user snippet with id ${id} not found, aborting`)
+  }
+}
+
+function getChangedValues<T extends Record<string, any>>(newValue: T, oldValue: T) {
+  const changedKeys = [];
+
+  for (const key in newValue) {
+    if (newValue.hasOwnProperty(key) && oldValue[key] !== newValue[key]) {
+      changedKeys.push(key);
+    }
+  }
+
+  return changedKeys;
+}
+
+function includesSome(array: string[], items: string[]) {
+  return items.some((item) => array.includes(item));
+}
+
+export function hasChanged<T extends Record<string, any>>(newValue: T, oldValue: T, keys: string[]) {
+  const changed = getChangedValues(newValue, oldValue);
+  return includesSome(changed, keys);
 }
