5881f37931ce3ac56f1bd56f29ddecd8e6582cd6
willow <42willow@pm.me>
2025-12-05 15:38:43 +1100

    feat(plugins/scrollSegments): hot reload

--- src/entrypoints/plugins/scrollSegments/index.ts
+++ src/entrypoints/plugins/scrollSegments/index.ts
@@ -1,19 +1,45 @@
-import { injectInlineStyles } from "@/utils";
+import { dataAttr, injectInlineStyles, setDataAttr, uninjectInlineStyles } from "@/utils";
 import { definePlugin } from "@/utils/plugin";
 import styleText from "./styles.css?inline";
 
+const ID = "scrollSegments";
+const PLUGIN_ID = `plugin-${ID}`;
+
 export default function init() {
   definePlugin(
-    "scrollSegments",
+    ID,
     () => {
-      const content = document.getElementById("content");
-      const footer = document.getElementById("footer");
+      const footerCopy = document.querySelector(dataAttr(PLUGIN_ID));
+      if (footerCopy) return;
+
+      // scroll to top to avoid hot reload bug
+      window.scrollTo({
+        top: 0,
+        behavior: "instant",
+      });
+
+      const content = document.querySelector("#content");
+      const footer = document.querySelector<HTMLDivElement>("#footer");
+
+      // add copy of footer to content
       if (content && footer) {
-        content.appendChild(footer);
+        const clone = footer.cloneNode(true) as HTMLDivElement;
+        setDataAttr(clone, PLUGIN_ID);
+        content.appendChild(clone);
       }
-      injectInlineStyles(styleText, "plugin-scrollSegments");
+
+      injectInlineStyles(styleText, PLUGIN_ID);
     },
-    () => {},
+    () => {
+      const footerCopy = document.querySelector(dataAttr(PLUGIN_ID));
+      if (!footerCopy) return;
+
+      // remove copy of footer from content
+      const content = document.querySelector("#content");
+      content?.removeChild(footerCopy);
+
+      uninjectInlineStyles(PLUGIN_ID);
+    },
     ["#content", "#footer"],
   );
 }
