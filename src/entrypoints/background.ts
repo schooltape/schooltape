schooltape/schooltape

commit 3db3c021a51f8e2401c2c6f0f838b3065a723876

fix(plugins/homepageSwitcher): closing tabs
* close #263
* `window.close()` -> `browser.tabs.remove()`

src/entrypoints/background.ts
@@ -1,6 +1,7 @@
 import type { Browser } from "#imports";
 import { browser, defineBackground, storage } from "#imports";
 import { logger } from "@/utils/logger";
+import type { BackgroundMessage } from "@/utils/storage";
 import { globalSettings, updated } from "@/utils/storage";
 import semver from "semver";
 
@@ -48,30 +49,26 @@ export default defineBackground(() => {
   // update icon when toggle or update is changed
   globalSettings.watch(updateIcon);
 
-  // listen for messages
-  interface Message {
-    resetSettings?: boolean;
-    inject?: string;
-    toTab?: string;
-    updateIcon?: boolean;
-  }
+  browser.runtime.onMessage.addListener(async (msg: BackgroundMessage, sender: Browser.runtime.MessageSender) => {
+    logger.child({ message: msg, sender }).info("[background] received message");
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  browser.runtime.onMessage.addListener(async (msg: any, sender: any) => {
-    const message = msg as Message;
-    logger.child({ message, sender }).info("[background] Received message");
-
-    if (message.resetSettings) {
-      resetSettings();
-    } else if (message.toTab) {
-      const tabs = await browser.tabs.query({ url: message.toTab });
-      if (tabs.length > 0) {
-        browser.tabs.update(tabs[0].id, { active: true });
-      } else if (sender.tab?.id) {
-        browser.tabs.update(sender.tab.id, { url: message.toTab });
-      }
-    } else if (message.updateIcon) {
-      updateIcon();
+    switch (msg.type) {
+      case "resetSettings":
+        resetSettings();
+        break;
+      case "updateIcon":
+        updateIcon();
+        break;
+      case "closeTab":
+        if (!sender.tab?.id) break;
+        browser.tabs.remove(sender.tab.id);
+        break;
+      case "updateTabUrl":
+        if (!sender.tab?.id) break;
+        browser.tabs.update(sender.tab.id, { url: msg.url });
+        break;
+      default:
+        logger.error(`[background] unknown message received: ${msg}`);
     }
 
     return true; // return success

src/entrypoints/plugins/homepageSwitcher/index.ts
@@ -1,4 +1,4 @@
-import { browser } from "#imports";
+import { sendMessage } from "@/utils";
 import { Plugin } from "@/utils/plugin";
 import type { Toggle } from "@/utils/storage";
 import type { StorageState } from "@/utils/storage/state.svelte";
@@ -40,9 +40,9 @@ export default new Plugin<Settings>(
           e.preventDefault();
 
           if (logos) {
-            const tab = logos[0].href;
-            if (closeCurrentTab.toggle) window.close(); // TODO:  Scripts may only close windows that were opened by a script.
-            browser.runtime.sendMessage({ toTab: tab });
+            const tabUrl = logos[0].href;
+            if (closeCurrentTab.toggle) sendMessage({ type: "closeTab" });
+            sendMessage({ type: "updateTabUrl", url: tabUrl });
           }
         },
         {

src/utils/storage/types.ts
@@ -1,3 +1,9 @@
+export type BackgroundMessage =
+  | { type: "resetSettings" }
+  | { type: "updateIcon" }
+  | { type: "closeTab" }
+  | { type: "updateTabUrl"; url: string };
+
 // global
 export interface Settings {
   global: boolean;

src/utils/index.ts
@@ -1,7 +1,7 @@
 import { browser } from "#imports";
 import { flavorEntries } from "@catppuccin/palette";
 import { logger } from "./logger";
-import type { LogoInfo } from "./storage";
+import type { BackgroundMessage, LogoInfo } from "./storage";
 import { globalSettings, schoolboxUrls } from "./storage";
 
 export const dataAttr = (id: string) => `[data-schooltape="${id}"]`;
@@ -12,6 +12,8 @@ export function setDataAttr(el: HTMLElement, id: string) {
 export async function onSchoolboxPage(): Promise<boolean> {
   return (await schoolboxUrls.get()).urls.includes(window.location.origin);
 }
+
+export const sendMessage = (msg: BackgroundMessage) => browser.runtime.sendMessage(msg);
 
 export function injectInlineStyles(styleText: string, id: string) {
   logger.info(`injecting styles with id ${id}`);

src/entrypoints/popup/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { flavors } from "@catppuccin/palette";
   import { globalSettings, updated } from "@/utils/storage";
-  import { browser, onMount } from "#imports";
+  import { onMount } from "#imports";
 
   import Router from "svelte-spa-router";
   import active from "svelte-spa-router/active";
@@ -9,6 +9,7 @@
   import Plugins from "./routes/Plugins.svelte";
   import Themes from "./routes/Themes.svelte";
   import Snippets from "./routes/Snippets.svelte";
+  import { sendMessage } from "@/utils";
 
   const routes = {
     "/": Home,
@@ -27,7 +28,7 @@
 
   onMount(() => {
     updated.update({ icon: false });
-    browser.runtime.sendMessage({ updateIcon: true });
+    sendMessage({ type: "updateIcon" });
   });
 </script>
 

src/entrypoints/popup/components/Footer.svelte
@@ -4,6 +4,7 @@
   import { updated } from "@/utils/storage";
   import Button from "./inputs/Button.svelte";
   import { RotateCcw, Globe, GitBranch } from "@lucide/svelte";
+  import { sendMessage } from "@/utils";
 
   let version = $state();
 
@@ -63,7 +64,7 @@
       classList="text-ctp-text hover:text-ctp-base hover:bg-ctp-red"
       onclick={() => {
         if (confirm("Are you sure you want to reset all settings?")) {
-          browser.runtime.sendMessage({ resetSettings: true });
+          sendMessage({ type: "resetSettings" });
           location.reload();
         }
       }}><RotateCcw size={22} /></Button>

src/entrypoints/start.content.ts
@@ -6,6 +6,7 @@ import {
   injectStylesheet,
   injectUserSnippet,
   onSchoolboxPage,
+  sendMessage,
   uninjectCatppuccin,
   uninjectStylesheet,
   uninjectUserSnippet,
@@ -93,7 +94,7 @@ export default defineContentScript({
       }
 
       // update icon
-      browser.runtime.sendMessage({ updateIcon: true });
+      sendMessage({ type: "updateIcon" });
     }
   },
 });

commit 9063adf0862b789eaff559dd1d738472cd830ae1

fix(plugins/homepageSwitcher): switching tab regression (#382)
fixes regression introduced by 3db3c02

---------

Co-authored-by: willow <42willow@pm.me>

src/entrypoints/background.ts
@@ -75,23 +75,21 @@ export default defineBackground(() => {
   browser.runtime.onMessage.addListener(async (msg: BackgroundMessage, sender: Browser.runtime.MessageSender) => {
     logger.info("[background] received message", { message: msg, sender });
 
-    switch (msg.type) {
-      case "resetSettings":
-        resetSettings();
-        break;
-      case "updateIcon":
-        updateIcon();
-        break;
-      case "closeTab":
-        if (!sender.tab?.id) break;
-        browser.tabs.remove(sender.tab.id);
-        break;
-      case "updateTabUrl":
-        if (!sender.tab?.id) break;
+    if (msg.type === "resetSettings") {
+      resetSettings();
+    } else if (msg.type === "updateIcon") {
+      updateIcon();
+    } else if (msg.type === "closeTab" && sender.tab?.id) {
+      browser.tabs.remove(sender.tab.id);
+    } else if (msg.type === "toTab") {
+      const toTab = await browser.tabs.query({ url: msg.url });
+      if (toTab.length > 0) {
+        browser.tabs.update(toTab[0].id, { active: true });
+      } else if (sender.tab?.id) {
         browser.tabs.update(sender.tab.id, { url: msg.url });
-        break;
-      default:
-        logger.error(`[background] unknown message received: ${msg}`);
+      }
+    } else {
+      logger.error(`[background] unknown message received: ${msg}`);
     }
 
     return true; // return success

src/entrypoints/plugins/homepageSwitcher/index.ts
@@ -42,7 +42,7 @@ export default new Plugin<Settings>(
           if (logos) {
             const tabUrl = logos[0].href;
             if (closeCurrentTab.toggle) sendMessage({ type: "closeTab" });
-            sendMessage({ type: "updateTabUrl", url: tabUrl });
+            sendMessage({ type: "toTab", url: tabUrl });
           }
         },
         {

src/utils/storage/types.ts
@@ -2,7 +2,7 @@ export type BackgroundMessage =
   | { type: "resetSettings" }
   | { type: "updateIcon" }
   | { type: "closeTab" }
-  | { type: "updateTabUrl"; url: string };
+  | { type: "toTab"; url: string };
 
 // global
 export interface SettingsV1 {